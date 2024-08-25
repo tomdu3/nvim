92950eee3a4beadfca88041e195bca8a9a143ce4
Tomislav Dukez <tomdu3@ymail.com>
2024-08-25 08:05:12 +0200

    feat(codeium): add codeium plugin

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,21 @@
+return {
+
+  -- original config
+  --    "Exafunction/codeium.vim",
+  --    event = "BufEnter",
+
+  { --workournd for the new update breaking codeium in neovim
+    "Exafunction/codeium.vim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    commit = "289eb724e5d6fab2263e94a1ad6e54afebefafb2",
+    event = "BufEnter",
+    config = function()
+      vim.keymap.set("i", "<C-Space>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+    end,
+  },
+}
