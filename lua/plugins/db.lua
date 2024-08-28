a3b149f996db19af851b54a290cdc829fae61b3f
Tomislav Dukez <tomdu3@ymail.com>
2024-08-28 15:39:11 +0200

    add vim-dadbod-ui

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -1,3 +1,24 @@
 return {
-  "tpope/vim-dadbod",
+  -- Main plugins
+  {
+    "tpope/vim-dadbod",
+    lazy = true, -- Make it load lazily
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      "tpope/vim-dadbod", -- Ensure vim-dadbod is loaded before vim-dadbod-ui
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
