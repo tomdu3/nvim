bf2a5cd9b61f4a062c7c54b1c852f8fe5f97b802
Tomislav Dukez <tomdu3@ymail.com>
2024-09-16 17:46:56 +0200

    update keymaps for different plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -53,3 +53,26 @@ end, opts)
 
 -- Map 'jk' to behave like Ctrl-C in Normal mode
 keymap.set("i", "jk", "<Esc>", opts)
+
+-- telescope
+vim.keymap.set("n", "<leader>fs", ":Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fp", ":Telescope git_files<cr>")
+vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>")
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<cr>")
+
+-- icon picker
+vim.keymap.set("n", "<leader>ic", ":IconPickerNormal<cr>", { noremap = true, silent = true })
+
+-- zen mode
+vim.keymap.set("n", "<leader>zm", ":ZenMode<cr>")
+
+-- format code using LSP
+-- vim.keymap.set("n", "<leader>pp", vim.lsp.buf.format)
+
+-- markdown preview
+vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>")
+
+-- nvim-comment
+vim.keymap.set({ "n", "v" }, "<leader>/", ":CommentToggle<cr>")
