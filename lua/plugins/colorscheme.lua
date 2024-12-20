718fc10980fc35f64276df34cd503780723418a0
Tomislav Dukez <tomdu3@ymail.com>
2024-12-20 13:56:17 +0000

    fix remove reduntant code in colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,82 @@
 return {
-  {
-    "sickill/vim-monokai", -- Monokai theme
-    lazy = false,          -- Load it eagerly
-    priority = 1000,       -- Load it early
-    config = function()
-      vim.cmd([[colorscheme monokai]])
-    end,
-  },
+  "navarasu/onedark.nvim",
+  config = function()
+    require("onedark").setup({
+      -- Main options --
+      style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+      transparent = false, -- Show/hide background
+      term_colors = true, -- Change terminal color as per the selected theme style
+      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+      -- toggle theme style ---
+      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+      toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+      -- Change code style ---
+      -- Options are italic, bold, underline, none
+      -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+      code_style = {
+        comments = "italic",
+        keywords = "none",
+        functions = "none",
+        strings = "none",
+        variables = "none",
+      },
+
+      -- Lualine options --
+      lualine = {
+        transparent = false, -- lualine center bar transparency
+      },
+
+      -- Custom Highlights --
+      colors = {}, -- Override default colors
+      highlights = {}, -- Override highlight groups
+
+      -- Plugins Config --
+      diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true, -- use undercurl instead of underline for diagnostics
+        background = true, -- use background color for virtual text
+      },
+    })
+    require("onedark").load() -- Load the theme
+  end,
 }
+
+--   {
+--   "projekt0n/github-nvim-theme",
+--   name = "github-theme",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require("github-theme").setup({
+--       -- ...
+--     })
+--
+--     vim.cmd("colorscheme github_dark")
+--   end,
+-- }
+
+--   {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent = true,
+--     }
+--   end,
+-- }
+--   {
+--     "sickill/vim-monokai", -- Monokai theme
+--     lazy = false,          -- Load it eagerly
+--     priority = 1000,       -- Load it early
+--     config = function()
+--       vim.cmd([[colorscheme monokai]])
+--     end,
+--   },
+-- }
 -- return {
 --   {
 --     "craftzdog/solarized-osaka.nvim",
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,9 +1,14 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup()
-    vim.keymap.set("n", "<leader>gj", ":Gitsigns next_hunk<cr>", {})
-    vim.keymap.set("n", "<leader>gk", ":Gitsigns prev_hunk<cr>", {})
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", {})
-  end,
+  { {
+    "tpope/vim-fugitive",
+  }, "tpope/vim-fugitive" },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gj", ":Gitsigns next_hunk<cr>", {})
+      vim.keymap.set("n", "<leader>gk", ":Gitsigns prev_hunk<cr>", {})
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", {})
+    end,
+  },
 }
