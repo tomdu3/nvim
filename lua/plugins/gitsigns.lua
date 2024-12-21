ed62767a42bc7f54ecaf54109fa4a747ae6a66b6
Tomislav Dukez <tomdu3@ymail.com>
2024-12-21 07:59:33 +0000

    add current line blame shortcut to gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -9,6 +9,7 @@ return {
       vim.keymap.set("n", "<leader>gj", ":Gitsigns next_hunk<cr>", {})
       vim.keymap.set("n", "<leader>gk", ":Gitsigns prev_hunk<cr>", {})
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<cr>", {})
     end,
   },
 }
