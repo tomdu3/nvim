f5b36f410c432e9278df512158fb1db4ca4de6bb
Tomislav Dukez <tomdu3@ymail.com>
2024-12-21 07:54:20 +0000

    set tokyonight default theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,49 +1,70 @@
 return {
-  "navarasu/onedark.nvim",
-  config = function()
-    require("onedark").setup({
-      -- Main options --
-      style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = false, -- Show/hide background
-      term_colors = true, -- Change terminal color as per the selected theme style
-      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-      -- toggle theme style ---
-      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-      toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
-
-      -- Change code style ---
-      -- Options are italic, bold, underline, none
-      -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-      code_style = {
-        comments = "italic",
-        keywords = "none",
-        functions = "none",
-        strings = "none",
-        variables = "none",
-      },
-
-      -- Lualine options --
-      lualine = {
-        transparent = false, -- lualine center bar transparency
-      },
-
-      -- Custom Highlights --
-      colors = {}, -- Override default colors
-      highlights = {}, -- Override highlight groups
-
-      -- Plugins Config --
-      diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true, -- use undercurl instead of underline for diagnostics
-        background = true, -- use background color for virtual text
-      },
-    })
-    require("onedark").load() -- Load the theme
-  end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    sidebars = { "qf", "help" },
+    day_brightness = 0.3,
+    hide_inactive_statusline = false,
+    lualine_bold = false,
+  },
 }
 
+-- one dark theme
+-- return {
+--   "navarasu/onedark.nvim",
+--   config = function()
+--     require("onedark").setup({
+--       -- Main options --
+--       style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+--       transparent = false, -- Show/hide background
+--       term_colors = true, -- Change terminal color as per the selected theme style
+--       ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+--       cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+--
+--       -- toggle theme style ---
+--       toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+--       toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+--
+--       -- Change code style ---
+--       -- Options are italic, bold, underline, none
+--       -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+--       code_style = {
+--         comments = "italic",
+--         keywords = "none",
+--         functions = "none",
+--         strings = "none",
+--         variables = "none",
+--       },
+--
+--       -- Lualine options --
+--       lualine = {
+--         transparent = false, -- lualine center bar transparency
+--       },
+--
+--       -- Custom Highlights --
+--       colors = {}, -- Override default colors
+--       highlights = {}, -- Override highlight groups
+--
+--       -- Plugins Config --
+--       diagnostics = {
+--         darker = true, -- darker colors for diagnostic
+--         undercurl = true, -- use undercurl instead of underline for diagnostics
+--         background = true, -- use background color for virtual text
+--       },
+--     })
+--     require("onedark").load() -- Load the theme
+--   end,
+-- }
+
+-- Github theme
+-- return {
 --   {
 --   "projekt0n/github-nvim-theme",
 --   name = "github-theme",
@@ -58,16 +79,8 @@ return {
 --   end,
 -- }
 
---   {
---   "folke/tokyonight.nvim",
---   lazy = false,
---   priority = 1000,
---   opts = function()
---     return {
---       transparent = true,
---     }
---   end,
--- }
+-- Monokai theme
+-- return {
 --   {
 --     "sickill/vim-monokai", -- Monokai theme
 --     lazy = false,          -- Load it eagerly
