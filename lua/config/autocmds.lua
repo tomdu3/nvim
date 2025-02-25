239371ab9575f4e0aa34b9d7557e22338416ee9f
Tomislav Dukez <tomdu3@ymail.com>
2025-02-25 07:24:36 +0000

    add config for jinja formatting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "jinja", "html" }, -- Add filetypes you want to format
+  callback = function()
+    vim.b.autoformat = true -- Enable auto-formatting for these filetypes
+  end,
+})
