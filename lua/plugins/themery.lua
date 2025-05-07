8ed31a10432da80776f40aebd07a1d9bc4b2a3f4
Tomislav Dukez <tomdu3@ymail.com>
2025-05-07 14:03:39 +0100

    add themery plugin

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -3,7 +3,16 @@ return {
   lazy = false,
   config = function()
     require("themery").setup({
-      themes = { "gruvbox", "ayu", "kanagawa-lotus", "kanagawa-dragon" }, -- Your list of installed colorschemes.
+      themes = {
+        "tokyonight-night",
+        "tokyonight-storm",
+        "tokyonight-day",
+        "tokyonight-moon",
+        "gruvbox",
+        "ayu",
+        "kanagawa-lotus",
+        "kanagawa-dragon",
+      }, -- Your list of installed colorschemes.
       livePreview = true,
     })
   end,
